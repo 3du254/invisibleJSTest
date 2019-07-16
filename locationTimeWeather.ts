7045a4f7f89e01665838e0e57d568b38df3170b0
mwalukomutunga <edwynkemboy@gmail.com>
2019-07-16 16:22:45 +0300

    created an array with Locations

--- locationTimeWeather.ts
+++ locationTimeWeather.ts
@@ -1,4 +1,11 @@
-interface Location{
-    name: string,
-    postalCode:string
+interface location {
+  name: string;
+  postalCode: string;
 }
+const locations: location[] = [
+  { name: "Nairobi", postalCode: "P.O BOX 100 Moi Avenue" },
+  { name: "Kitale", postalCode: "P.O BOX 2001 Kenyatta Street" },
+  { name: "Mombasa", postalCode: "P.O BOX 1001 Mombasa" }
+];
+const logTimeWeather = locations.map(company => company);
+console.log(logTimeWeather);
